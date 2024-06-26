a7118d3347578f1ad590391757e9e0b68d48da59
Liu <tel17684311578@163.com>
2024-06-26 16:09:37 +0800

    打包Dcatadmin文档

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminTablesSeeder::class);
+        $this->call(DocumentsSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
