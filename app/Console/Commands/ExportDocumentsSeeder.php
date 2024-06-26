df6931bcf4976989550e56a0856d82e0ecf35ed3
Liu <tel17684311578@163.com>
2024-06-26 16:23:18 +0800

    修复导出文档类型错误重新导出DCATADMIN文档

--- app/Console/Commands/ExportDocumentsSeeder.php
+++ app/Console/Commands/ExportDocumentsSeeder.php
@@ -38,25 +38,34 @@ class ExportDocumentsSeeder extends Command
 
     protected function varExport($var, $indent = '        ')
     {
-        switch (gettype($var)) {
-            case 'string':
-                return '"' . addcslashes($var, "\\\$\"\r\n\t\v\f") . '"';
-            case 'array':
-                $indexed = array_keys($var) === range(0, count($var) - 1);
-                $r = [];
-                foreach ($var as $key => $value) {
-                    $r[] = "$indent    "
-                        . ($indexed ? '' : $this->varExport($key) . ' => ')
-                        . $this->varExport($value, "$indent    ");
+        if (is_array($var)) {
+            $r = [];
+            foreach ($var as $item) {
+                if (is_object($item)) {
+                    $item = (array) $item;  // 转换对象为数组
                 }
-                return "[\n" . implode(",\n", $r) . "\n$indent]";
-            case 'boolean':
-                return $var ? 'true' : 'false';
-            case 'integer':
-            case 'double':
-                return $var;
-            default:
-                return var_export($var, true);
+                $itemParts = [];
+                foreach ($item as $key => $value) {
+                    if (is_string($value)) {
+                        $value = '"' . addcslashes($value, "\\\$\"\r\n\t\v\f") . '"';
+                    } elseif (is_bool($value)) {
+                        $value = $value ? 'true' : 'false';
+                    } elseif ($value === null) {
+                        $value = 'null';
+                    }
+                    $itemParts[] = "'$key' => $value";
+                }
+                $r[] = "$indent    [" . implode(", ", $itemParts) . "]";
+            }
+            return "[\n" . implode(",\n", $r) . "\n$indent]";
+        } elseif (is_string($var)) {
+            return '"' . addcslashes($var, "\\\$\"\r\n\t\v\f") . '"';
+        } elseif (is_bool($var)) {
+            return $var ? 'true' : 'false';
+        } elseif ($var === null) {
+            return 'null';
+        } else {
+            return $var;
         }
     }
 }
